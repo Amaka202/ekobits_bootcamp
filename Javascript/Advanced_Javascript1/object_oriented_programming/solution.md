c320e6300e3557019287a2fa3f21de6aa56bd6fb
Umeh Chiamaka <amakaumeh01@gmail.com>
2020-10-24 11:51:21 +0100

    completed exercises for this keyword

---
+++ Javascript/Advanced_Javascript1/object_oriented_programming/solution.md
@@ -0,0 +1,7 @@
+## Define encapsulation, abstraction, and inheritance in your own words
+
+**encapsulations** is a situation whereby data are encapsulated into a class such that other functions or classes cannot directly change the data.
+
+**abstraction** is a way of hiding the implementation details and showing only the functionality to the users. In other words, it ignores the irrelevant details and shows only the required one.
+
+**inheritance** is when a child class inherits functionality from a parent class. 
--- Javascript/Advanced_Javascript1/this_keyword_exrcises/script.js
+++ Javascript/Advanced_Javascript1/this_keyword_exrcises/script.js
@@ -85,7 +85,106 @@ var instructor = {
     },
 }
 
-instructor.info.data.logLocation()
+// instructor.info.data.logLocation()
 
 // output: undefined
 // logLocation is not a function
+
+// Fix the following code:
+
+var obj = {
+    fullName: "Harry Potter",
+    person: {
+        
+        sayHi: function(){
+            return "This person's name is " + this.fullName
+        }
+    }
+}
+
+// fixed:
+
+var obj = {
+    fullName: "Harry Potter",
+    person: {
+        sayHi: function(){
+            return "This person's name is " + obj.fullName
+        }
+    }
+}
+
+// List two examples of "array-like-objects" that we have seen.
+
+// 1) aruguments
+// 2) DOM node lists
+
+
+// Write a function called sumEvenArguments which takes all of the arguments passed to a function and returns the sum of the even ones.
+
+function sumEvenArguments(args) {
+    args = [...arguments];
+    let sum = 0;
+    args.forEach(val => {
+        if(val % 2 == 0){
+            sum += val;
+        }
+    })
+    return sum;
+}
+
+// Write a function called arrayFrom which converts an array-like-object into an array.
+
+function arrayFrom(args) {
+    args = [...arguments];
+    return args;
+}
+
+
+// Write a function called invokeMax which accepts a function and a maximum amount. invokeMax should return a function that when called increments a counter. If the counter is greater than the maximum amount, the inner function should return "Maxed Out!"
+
+function invokeMax(fn, maxNum) {
+    let counter = 0;
+    return function(){
+        counter++;
+        if(counter > maxNum){
+            return "Maxed Out!";
+        }else{
+            return fn();
+        }
+    }
+}
+
+function add(a,b){
+    return a+b
+}
+
+
+var addOnlyThreeTimes = invokeMax(add,3);
+
+// Write a function called guessingGame which takes in one parameter amount. The function should return another function that takes in a parameter called guess. In the outer function, you should create a variable called answer which is the result of a random number between 0 and 10 as well as a variable called guesses which should be set to 0.
+
+// In the inner function, if the guess passed in is the same as the random number (defined in the outer function) - you should return the string "You got it!". If the guess is too high return "You're too high!" and if it is too low, return "You're too low!". You should stop the user from guessing if the amount of guesses they have made is greater than the initial amount passed to the outer function.
+
+// You will have to make use of closure to solve this problem.
+
+function guessingGame(amount) {
+    let answer = Math.floor(Math.random() * 11);
+    let guesses = 0;
+    return function (guess) {
+        guesses ++;
+        if(guess == answer && guesses !== amount){
+            return "You got it!";
+        }else if(guess > answer && guesses !== amount){
+            return "You're too high!"
+        }else if(guess < answer && guesses !== amount){
+            return "You're too low!"
+        }else{
+            return "You are all done playing!"
+        }
+
+       
+    }
+
+}
+
+let game = guessingGame(3);
