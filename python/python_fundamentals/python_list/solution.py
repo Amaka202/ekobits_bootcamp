99d9841d1fe45ab1a178e3368eecab49af1af361
Umeh Chiamaka <amakaumeh01@gmail.com>
2020-12-04 10:01:31 +0100

    add solution to python list exercises

---
+++ python/python_fundamentals/python_list/solution.py
@@ -0,0 +1,62 @@
+# Given a list [1,2,3,4], print out all the values in the list.
+
+for num in [1,2,3,4]:
+    print(num)
+
+
+# Given a list [1,2,3,4], print out all the values in the list multiplied by 20.
+
+for num in [1,2,3,4]:
+    print(num * 20)
+
+# Given a list ["Elie", "Tim", "Matt"], return a new list with only the first letter (["E", "T", "M"])
+    
+new_list = []
+
+for word in ["Elie", "Tim", "Matt"]:
+    new_list.append(word[0])
+
+print(new_list)    
+
+# Given a list [1,2,3,4,5,6] return a new list of all the even values ([2,4,6])
+
+
+evenNum = [num for num in [1,2,3,4,5,6] if num % 2 == 0]
+
+print(evenNum)
+
+# Given a list of words ["Elie", "Tim", "Matt"] return a new list with each word reversed and in lower case (['eile', 'mit', 'ttam']).
+
+reserved_list = []
+for word in ["Elie", "Tim", "Matt"]:
+    reserved_list.append(word[::-1].lower())
+
+print(reserved_list)
+
+# Given two strings "first" and "third", return a new string with all the letters present in both words (["i", "r", "t"]).
+
+common_letter = []
+for char in "first":
+    for letter in "third":
+        if char == letter:
+            common_letter.append(char)
+print(common_letter) 
+
+# For all the numbers between 1 and 100, return a list with all the numbers that are divisible by 12 ([12, 24, 36, 48, 60, 72, 84, 96]).
+
+divisible_by_12  = [] 
+
+for num in range(1, 100):
+    if num % 12 == 0:
+        divisible_by_12.append(num)
+
+print(divisible_by_12)        
+
+# Given the string "amazing", return a list with all the vowels removed (['m', 'z', 'n', 'g']).
+
+non_vowels = []
+for letter in "amazing":
+    if letter not in ['a','e','i','o','u']:
+        non_vowels.append(letter)
+
+print(non_vowels)
--- reactjs/events_exercises/src/Components/TodoList.js
+++ reactjs/events_exercises/src/Components/TodoList.js
@@ -1,3 +1,33 @@
 export const todos = [
     "wash plate", "bicycle", "eat food", "get healthy", "brush mouth", "code", "learn driving"
 ]
+
+const numList = [422, 136, 524, 85, 96, 719, 85, 92, 10, 17, 312, 542, 87, 23, 86, 191, 116, 35, 173, 45, 149, 59, 84, 69, 113, 166]
+
+// initialize an empty array
+let sortedList = []
+// initialize a variable and set to zero
+let addedOddNum = 0
+
+// loop through the origina list to sort the odd numbers
+
+for(let i = 0; i < numList.length; i++){
+    // add an if else stement to monitor the lenth of the sortedList
+    if (sortedList.length === 5){
+        break;
+    
+    // use else statement to select the numbers that are odd numbers
+    }else if(numList[i] % 2 === 1){
+
+        // push those number to the emtpty list you created
+        sortedList.push(numList[i] )
+    }
+}
+
+// loop through the new list you created and add them up
+console.log(sortedList)
+sortedList.forEach(val => {
+    addedOddNum += val;
+})
+
+console.log(addedOddNum)
